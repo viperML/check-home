32c7a6e5fc97e08c95726dba6e51cd342f3bfb88
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-05-22 10:40:58 +0200

    add more info

--- main.c
+++ main.c
@@ -4,17 +4,27 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char **argv) {
-  {
-    struct passwd *pw = getpwuid(getuid());
-    const char *homedir = pw->pw_dir;
+int main(int argc, char** argv) {
+  printf("https://man7.org/linux/man-pages/man1/login.1.html\n");
+  printf("Discrepancies on the values from the password database and\n");
+  printf("env variables may be due to improper system configuration.\n\n");
 
-    printf("pw:\t%s\n", homedir);
+  {
+    struct passwd* pw = getpwuid(getuid());
+
+    printf("=> Password dabase:\n");
+    printf("name:\t%s\n", pw->pw_name);
+    printf("home:\t%s\n", pw->pw_dir);
+    printf("shell:\t%s\n", pw->pw_shell);
   }
 
+  printf("\n");
+
   {
-    char *homedir = getenv("HOME");
-    printf("env:\t%s\n", homedir);
+    printf("=> Environment variables\n");
+    printf("name:\t%s\n", getenv("USER"));
+    printf("home:\t%s\n", getenv("HOME"));
+    printf("shell:\t%s\n", getenv("SHELL"));
   }
 
   return EXIT_SUCCESS;
